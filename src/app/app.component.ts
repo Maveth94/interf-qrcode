4e47de912e064a8f61890867cc6ecee14c4e9f67
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-25 16:01:29 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class monum {
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   //monumArr: Array<monum> = [];
