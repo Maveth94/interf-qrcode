3ffdecfab9ff2cf57ff66285dfd15baad4163286
Maveth94 <giovannigiuliodori@outlook.com>
2021-05-26 09:12:14 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ModificaUrlComponent } from './modificaUrl/modificaUrl.component';
 import { AggiungiComponent } from './aggiungi/aggiungi.component';
+import { ModificaInfoComponent } from './modifica-info/modifica-info.component';
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, ModificaUrlComponent, AggiungiComponent],
+  declarations: [AppComponent, ModificaUrlComponent, AggiungiComponent, ModificaInfoComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/modifica-info/modifica-info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modifica-info',
+  templateUrl: './modifica-info.component.html',
+  styleUrls: ['./modifica-info.component.css']
+})
+export class ModificaInfoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
